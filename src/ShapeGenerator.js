a9e6c6cb1521b787f0df6d629d7275d0758d4f72
Sune Simonsen <sune@we-knowhow.dk>
2018-08-10 22:11:23 +0200

    Added a shape generator

---
+++ src/ShapeGenerator.js
@@ -0,0 +1,96 @@
+const Generator = require("./Generator");
+const ConstantGenerator = require("./ConstantGenerator");
+const WeightedGenerator = require("./WeightedGenerator");
+const unwrap = require("./unwrap");
+
+const containsGeneratorsMatching = (shape, predicate) => {
+  if (!shape) {
+    return false;
+  } else if (Array.isArray(shape)) {
+    return shape.some(item => containsGeneratorsMatching(item, predicate));
+  } else if (shape.isGenerator) {
+    return predicate(shape);
+  } else if (typeof shape === "object") {
+    return Object.keys(shape).some(key =>
+      containsGeneratorsMatching(shape[key], predicate)
+    );
+  } else {
+    return false;
+  }
+};
+
+const shrink = (shape, generated) => {
+  if (!shape) {
+    return shape;
+  } else if (Array.isArray(shape)) {
+    return shape.map((item, i) => shrink(item, generated[i]));
+  } else if (shape.isGenerator) {
+    return shape.shrink ? shape.shrink(generated) : shape;
+  } else if (typeof shape === "object") {
+    return Object.keys(shape).reduce((result, key) => {
+      result[key] = shrink(shape[key], generated[key]);
+      return result;
+    }, {});
+  } else {
+    return shape;
+  }
+};
+
+const expand = (shape, generated) => {
+  if (!shape) {
+    return shape;
+  } else if (Array.isArray(shape)) {
+    return shape.map((item, i) => expand(item, generated[i]));
+  } else if (shape.isGenerator) {
+    return shape.expand ? shape.expand(generated) : shape;
+  } else if (typeof shape === "object") {
+    return Object.keys(shape).reduce((result, key) => {
+      result[key] = expand(shape[key], generated[key]);
+      return result;
+    }, {});
+  } else {
+    return shape;
+  }
+};
+
+class ShapeGenerator extends Generator {
+  constructor(shape) {
+    super("shape", shape);
+  }
+
+  shrink(generated) {
+    const isShrinkable = containsGeneratorsMatching(this.options, generator =>
+      Boolean(generator.shrink)
+    );
+
+    if (isShrinkable) {
+      return new ShapeGenerator(shrink(this.options, generated));
+    } else {
+      return new ConstantGenerator(generated);
+    }
+  }
+
+  expand(generated) {
+    const isExpandable = containsGeneratorsMatching(this.options, generator =>
+      Boolean(generator.expand)
+    );
+
+    if (isExpandable) {
+      return new WeightedGenerator([
+        [new ShapeGenerator(expand(this.options, generated)), 1],
+        [new ConstantGenerator(generated), 1.5]
+      ]);
+    } else {
+      return new WeightedGenerator([
+        [this, 1],
+        [new ConstantGenerator(generated), 1.5]
+      ]);
+    }
+  }
+
+  generate(chance) {
+    return unwrap(this.options, chance);
+  }
+}
+
+module.exports = ShapeGenerator;
---
+++ src/ShapeGenerator.spec.js
@@ -0,0 +1,219 @@
+const expect = require("../test/expect");
+const ShapeGenerator = require("./ShapeGenerator");
+const StringGenerator = require("./StringGenerator");
+const IntegerGenerator = require("./IntegerGenerator");
+const ArrayGenerator = require("./ArrayGenerator");
+const chanceCache = require("./chanceCache");
+
+describe("ShapeGenerator", () => {
+  beforeEach(() => {
+    chanceCache.clear();
+  });
+
+  let generator;
+  beforeEach(() => {
+    generator = new ShapeGenerator({
+      nested: {
+        numbers: [
+          new IntegerGenerator({ min: 0, max: 100 }),
+          new IntegerGenerator({ min: 500, max: 1000 })
+        ],
+        strings: [new StringGenerator(), new StringGenerator({ max: 5 })]
+      },
+      arrays: new ArrayGenerator(new IntegerGenerator({ min: 0, max: 100 }), {
+        max: 5
+      })
+    });
+  });
+
+  it("unwraps all of the generators in the given structure", () => {
+    expect(generator, "to yield items", [
+      {
+        nested: {
+          numbers: [37, 899],
+          strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+        },
+        arrays: [53]
+      },
+      {
+        nested: {
+          numbers: [61, 700],
+          strings: ["v[Br", "6T"]
+        },
+        arrays: [38]
+      },
+      {
+        nested: {
+          numbers: [51, 992],
+          strings: ["d@SYmHea(*)P7Cwbhr", "GYjT"]
+        },
+        arrays: [84, 3]
+      },
+      {
+        nested: {
+          numbers: [17, 955],
+          strings: ["nX3xFMpOQnc)", "H*D%&"]
+        },
+        arrays: [73, 93, 32]
+      },
+      {
+        nested: {
+          numbers: [8, 785],
+          strings: ["d)y!C3", "9"]
+        },
+        arrays: [36, 97, 28]
+      }
+    ]);
+  });
+
+  describe("shrink", () => {
+    it("returns a new shape generator where all generators in the structure has been shunken", () => {
+      const value = generator.take(1)[0];
+      expect(generator.shrink(value), "to satisfy", {
+        isGenerator: true,
+        generatorName: "shape",
+        options: {
+          nested: {
+            numbers: [
+              {
+                generatorName: "integer",
+                options: { min: 0, max: 37 }
+              },
+              {
+                generatorName: "integer",
+                options: { min: 500, max: 899 }
+              }
+            ],
+            strings: [
+              {
+                generatorName: "stringSplicer",
+                options: {
+                  text: "25SSlGlheH#ySk0Wbe)19*pan]nTw",
+                  min: 0
+                }
+              },
+              {
+                generatorName: "stringSplicer",
+                options: {
+                  text: "aFb",
+                  min: 0
+                }
+              }
+            ]
+          },
+          arrays: {
+            generatorName: "arraySplicer",
+            options: {
+              items: [
+                { generatorName: "integer", options: { min: 0, max: 53 } }
+              ],
+              min: 0
+            }
+          }
+        }
+      });
+    });
+
+    it("shrinks all of the generators in the shape with regards to the given structure", () => {
+      const value = generator.take(1)[0];
+      expect(generator.shrink(value), "to yield items", [
+        {
+          nested: {
+            numbers: [23, 659],
+            strings: ["25SSGlheH#ySk0Wbe)19*pan]nTw", "a"]
+          },
+          arrays: [24]
+        },
+        {
+          nested: {
+            numbers: [3, 814],
+            strings: ["25SSlGlheH#ySk0Wbe9*pan]nTw", "ab"]
+          },
+          arrays: [25]
+        },
+        {
+          nested: {
+            numbers: [1, 843],
+            strings: ["25SSlGlheH#ySk0WbenTw", "Fb"]
+          },
+          arrays: [51]
+        },
+        {
+          nested: {
+            numbers: [35, 886],
+            strings: ["25SSlGlheH#ySk0WTw", "aFb"]
+          },
+          arrays: [12]
+        },
+        {
+          nested: { numbers: [26, 596], strings: ["25SSlGlheH#ySn]nTw", "b"] },
+          arrays: []
+        }
+      ]);
+    });
+
+    it("shrinks towards the minimal structure", () => {
+      expect(generator, "to shrink towards", {
+        nested: { numbers: [0, 500], strings: ["", ""] },
+        arrays: []
+      });
+    });
+  });
+
+  describe("expand", () => {
+    it("expands all of the generators in the shape with regards to the given structure", () => {
+      const value = generator.take(1)[0];
+      expect(generator.expand(value), "to yield items", [
+        {
+          nested: {
+            numbers: [37, 899],
+            strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+          },
+          arrays: [53]
+        },
+        {
+          nested: {
+            numbers: [37, 899],
+            strings: ["25SSlGlheH#HSk0Wbe)19*pan]nTw", "taFbx"]
+          },
+          arrays: [97]
+        },
+        {
+          nested: {
+            numbers: [37, 899],
+            strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+          },
+          arrays: [53]
+        },
+        {
+          nested: {
+            numbers: [37, 899],
+            strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+          },
+          arrays: [53]
+        },
+        {
+          nested: {
+            numbers: [37, 899],
+            strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+          },
+          arrays: [53]
+        },
+        {
+          nested: {
+            numbers: [37, 899],
+            strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+          },
+          arrays: [53]
+        },
+        {
+          nested: {
+            numbers: [93, 945],
+            strings: ["25SSlGlheH#ySk0Wbe)19*pan]nTw", "aFb"]
+          },
+          arrays: [53, 76]
+        }
+      ]);
+    });
+  });
+});
