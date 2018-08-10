9d8927bdbcb7ca2ae506437a2a2f2da37838765d
Sune Simonsen <sune@we-knowhow.dk>
2018-08-10 22:07:32 +0200

    Added a pickset generator

---
+++ src/PicksetGenerator.js
@@ -0,0 +1,101 @@
+const Generator = require("./Generator");
+const ConstantGenerator = require("./ConstantGenerator");
+
+class PicksetGenerator extends Generator {
+  constructor(items = [], { min = 0, max = items.length } = {}) {
+    super("pickset", {
+      items,
+      min,
+      max
+    });
+
+    if (items.length === 0) {
+      throw new Error("Pickset require a non-empty array of items");
+    }
+  }
+
+  shrink(items) {
+    if (items.length === 0) {
+      return new ConstantGenerator([]);
+    }
+
+    const shrinkableData =
+      items.length < 10 &&
+      items === this.lastUnwrappedValue &&
+      (this.lastValue || []).some(g => g && g.shrink);
+
+    let shrinkable = items.length < this.options.max || shrinkableData;
+
+    if (!shrinkable) {
+      return new ConstantGenerator(items);
+    }
+
+    if (shrinkableData) {
+      items = this.lastValue.map(
+        (g, i) => (g && g.shrink ? g.shrink(items[i]) : items[i])
+      );
+    } else {
+      items = this.lastValue;
+    }
+
+    return new PicksetGenerator(items, {
+      min: this.options.min,
+      max: items.length
+    });
+  }
+
+  expand(data) {
+    return this.map((items, chance) => {
+      const margin = Math.max(
+        Math.min(
+          Math.floor((items.length - data.length) / 2),
+          Math.ceil(data.length * 0.3)
+        ),
+        0
+      );
+
+      const includeLeftMargin = chance.bool({ likelihood: 70 });
+      const includeRightMargin = chance.bool({ likelihood: 70 });
+
+      const marginLength =
+        (includeLeftMargin ? margin : 0) + (includeRightMargin ? margin : 0);
+
+      const result = new Array(marginLength + data.length);
+
+      let i = 0;
+      if (includeLeftMargin) {
+        while (i < margin) {
+          result[i] = items[i++];
+        }
+      }
+
+      for (let j = 0; j < data.length; i++, j++) {
+        result[i] = chance.bool({ likelihood: 95 })
+          ? data[j]
+          : items[i % items.length];
+      }
+
+      if (includeRightMargin) {
+        while (i < result.length) {
+          result[i] = items[i++];
+        }
+      }
+
+      return result;
+    });
+  }
+
+  generate(chance) {
+    const { items, min, max } = this.options;
+
+    this.lastValue = chance.pickset(items, chance.natural({ min, max }));
+
+    this.lastUnwrappedValue = this.lastValue.map(
+      item => (item && item.isGenerator ? item.generate(chance) : item)
+    );
+
+    return this.lastUnwrappedValue;
+  }
+}
+
+module.exports = PicksetGenerator;
---
+++ src/PicksetGenerator.spec.js
@@ -0,0 +1,227 @@
+const expect = require("../test/expect");
+const PicksetGenerator = require("./PicksetGenerator");
+const IntegerGenerator = require("./IntegerGenerator");
+const chanceCache = require("./chanceCache");
+
+describe("PicksetGenerator", () => {
+  beforeEach(() => {
+    chanceCache.clear();
+  });
+
+  let generator;
+
+  describe("when given no limits", () => {
+    beforeEach(() => {
+      generator = new PicksetGenerator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+
+    describe("when given no arguments", () => {
+      it("fails", () => {
+        expect(
+          () => {
+            new PicksetGenerator(); // eslint-disable-line no-new
+          },
+          "to throw",
+          "Pickset require a non-empty array of items"
+        );
+      });
+    });
+
+    it("create arrays by picking subsets of the given items", () => {
+      expect(generator, "to yield items", [[7, 8, 1, 5], [0], [8, 3, 4, 1, 7]]);
+    });
+
+    describe("shrink", () => {
+      it("shrinks towards the empty array", () => {
+        expect(generator, "to shrink towards", []);
+      });
+    });
+
+    describe("expand", () => {
+      it("expands the given data", () => {
+        expect(generator.expand([7, 9, 1, 6]), "to yield items", [
+          [7, 9, 1, 6],
+          [7, 7, 1, 6],
+          [2, 0, 7, 9, 1, 6, 5, 4],
+          [7, 9, 1, 6],
+          [6, 1, 7, 9, 1, 6, 8, 0],
+          [9, 5, 7, 9, 1, 6, 4, 3],
+          [7, 9, 1, 6],
+          [7, 9, 1, 6, 8, 2],
+          [7, 9, 1, 6, 3],
+          [3, 8, 9, 1, 6, 1]
+        ]);
+      });
+    });
+  });
+
+  describe("when given a minimum", () => {
+    beforeEach(() => {
+      generator = new PicksetGenerator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], {
+        min: 3
+      });
+    });
+
+    it("create arrays by picking subsets larger than or equal to the given minimum size", () => {
+      expect(
+        generator,
+        "to yield items satisfying",
+        "to have length satisfying",
+        "to be within",
+        generator.options.min,
+        generator.options.max
+      );
+    });
+
+    describe("shrink", () => {
+      it("shrinks towards an array of minimum length", () => {
+        expect(generator, "to shrink towards", [1, 8, 7]);
+      });
+    });
+
+    describe("expand", () => {
+      it("honor the constraints", () => {
+        expect(
+          generator.expand([7, 9, 1, 6]),
+          "to yield items satisfying",
+          "to have length satisfying",
+          "to be within",
+          generator.options.min,
+          generator.options.max
+        );
+      });
+    });
+  });
+
+  describe("when given a maximum", () => {
+    beforeEach(() => {
+      generator = new PicksetGenerator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], {
+        max: 6
+      });
+    });
+
+    it("create arrays by picking subsets that is less than or equal to given maximum size", () => {
+      expect(
+        generator,
+        "to yield items satisfying",
+        "to have length satisfying",
+        "to be within",
+        generator.options.min,
+        generator.options.max
+      );
+    });
+
+    describe("expand", () => {
+      it("honor the constraints", () => {
+        expect(
+          generator.expand([7, 9, 1, 6]),
+          "to yield items satisfying",
+          "to have length satisfying",
+          "to be within",
+          generator.options.min,
+          generator.options.max
+        );
+      });
+    });
+  });
+
+  describe("when given a minimum and a maximum", () => {
+    beforeEach(() => {
+      generator = new PicksetGenerator([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], {
+        min: 3,
+        max: 6
+      });
+    });
+
+    it("create arrays by picking subsets that within the minimum and maximum sizes", () => {
+      expect(
+        generator,
+        "to yield items satisfying",
+        "to have length satisfying",
+        "to be within",
+        generator.options.min,
+        generator.options.max
+      );
+    });
+
+    describe("expand", () => {
+      it("honor the constraints", () => {
+        expect(
+          generator.expand([7, 9, 1, 6]),
+          "to yield items satisfying",
+          "to have length satisfying",
+          "to be within",
+          generator.options.min,
+          generator.options.max
+        );
+      });
+    });
+  });
+
+  describe("when the items contains generators", () => {
+    const itemGenerator = new IntegerGenerator({ min: 0, max: 100 });
+
+    beforeEach(() => {
+      generator = new PicksetGenerator([
+        itemGenerator,
+        itemGenerator,
+        itemGenerator,
+        itemGenerator
+      ]);
+    });
+
+    it("uses the generators to generator values for the resulting arrays", () => {
+      expect(generator, "to yield items", [[96], [], [45, 15, 10]]);
+    });
+
+    describe("and has a minimum", () => {
+      beforeEach(() => {
+        generator = new PicksetGenerator(
+          [
+            itemGenerator,
+            itemGenerator,
+            itemGenerator,
+            itemGenerator,
+            itemGenerator,
+            itemGenerator
+          ],
+          { min: 3 }
+        );
+      });
+
+      describe("shrink", () => {
+        it("shrinks towards an array of minimum length and minimum items", () => {
+          expect(generator, "to shrink towards", [0, 0, 0]);
+        });
+      });
+
+      describe("expand", () => {
+        it("uses the generators", () => {
+          expect(generator.expand([7, 9, 1, 6]), "to yield items", [
+            [7, 9, 1, 6],
+            [84, 9, 1, 6],
+            [7, 9, 1, 6],
+            [7, 9, 1, 68],
+            [7, 9, 1, 6],
+            [52, 9, 1, 6],
+            [7, 9, 1, 6, 52],
+            [7, 27, 1, 6],
+            [7, 9, 1, 6],
+            [7, 9, 1, 6]
+          ]);
+        });
+
+        it("honor the constraints", () => {
+          expect(
+            generator.expand([7, 9, 1, 6]),
+            "to yield items satisfying",
+            "to have length satisfying",
+            "to be within",
+            generator.options.min,
+            generator.options.max
+          );
+        });
+      });
+    });
+  });
+});
