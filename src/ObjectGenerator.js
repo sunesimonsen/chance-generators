c23fe9aa67a31c421bce7fdbe11abd337094da8d
Sune Simonsen <sune@we-knowhow.dk>
2018-11-26 22:02:38 +0100

    Updated site

---
+++ src/ObjectGenerator.js
@@ -0,0 +1,9 @@
+const Generator = require("./Generator");
+
+class ObjectGenerator extends Generator {
+  generate(chance, context) {
+    console.log("wat");
+  }
+}
+
+module.exports = ObjectGenerator;
