4b7c29330ab8271d4fbbfbb1eddbce7596b5b725
Andreas Lind <andreaslindpetersen@gmail.com>
2019-09-27 00:13:40 +0200

    unwrap: Let non-plain objects through, add a few tests

--- src/unwrap.js
+++ src/unwrap.js
@@ -5,7 +5,7 @@ const unwrap = (shape, chance, context) => {
     return shape.map(item => unwrap(item, chance, context));
   } else if (shape.isGenerator) {
     return shape.generate(chance, context);
-  } else if (shape && typeof shape === "object") {
+  } else if (typeof shape === "object" && shape.constructor === Object) {
     return Object.keys(shape).reduce((result, key) => {
       result[key] = unwrap(shape[key], chance, context);
       return result;
