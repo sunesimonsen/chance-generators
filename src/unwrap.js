b36faa5b5693c86e87942a1de62ae7da5ce2ce49
Andreas Lind <andreaslindpetersen@gmail.com>
2019-09-27 00:11:21 +0200

    unwrap: Let null through despite typeof null === 'object'

--- src/unwrap.js
+++ src/unwrap.js
@@ -5,7 +5,7 @@ const unwrap = (shape, chance, context) => {
     return shape.map(item => unwrap(item, chance, context));
   } else if (shape.isGenerator) {
     return shape.generate(chance, context);
-  } else if (typeof shape === "object") {
+  } else if (shape && typeof shape === "object") {
     return Object.keys(shape).reduce((result, key) => {
       result[key] = unwrap(shape[key], chance, context);
       return result;
